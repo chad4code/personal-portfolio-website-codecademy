f8364fb718bebeae0a5b2a66127a92debd110d1f
chadmj <106312278+chad4code@users.noreply.github.com>
2023-01-03 17:17:06 +0200

    Fix Display Opt and Study Topics

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,7 @@
 // Global variables
 let aboutLearning = document.getElementById('about-learning');
 let manBoxClicked = false;
-let hBoxClicked = false;
+// let hBoxClicked = false;
 let stickManContainer = document.getElementById('stick-man-container');
 stickManContainer.style.display = 'none';
 let topics = document.getElementsByTagName('h4');
@@ -29,7 +29,7 @@ function changeColorTheme() {
     explainBox.style.color = 'rgb(64, 64, 64)';
 }
 
-const revertColorTheme = () => {
+/*const revertColorTheme = () => {
     aboutLearning.style.backgroundColor = 'rgb(211, 211, 211)';
     aboutLearning.style.color = 'rgba(0, 0, 205, 0.9)';
     for (const element of hBox) {
@@ -40,52 +40,51 @@ const revertColorTheme = () => {
 }
 
 for (item of hBox) {
-    item.onmousedown = () => {
-        hBoxClicked = true;
-        console.log('hBox.onmousedown');
-    }
-}
+    item.addEventListener("click", () => {
+        hBoxClicked = !hBoxClicked;
+        console.log('hBox.click');
+    })
+} */
+
 aboutLearning.onmousedown = () => {
+    // let hBoxClicked = false;
     if (manBoxClicked === false) {
         stickManContainer.style.display = 'flex';
         stickManContainer.style.justifyContent = 'center';
         stickManContainer.style.padding = '3em';
         changeColorTheme();
-    } else if (manBoxClicked === true && hBoxClicked === false) {
+    } /* else if (manBoxClicked === true && hBoxClicked === false) {
         stickManContainer.style.display = 'none';
         revertColorTheme();
         console.log('aboutLearning.onmousedown else if');
-    }
-    manBoxClicked = !manBoxClicked;
+    }*/
+    manBoxClicked = true;
 }
 
-// explain box functionality
-const displayOpt = () => {
-    if (explainBox.style.height > '15em') {
+// explain box functionality: redone in styles.css
+/* const displayOpt = () => {
+    const explainBoxHeight = explainBox.style.height;
+    console.log(explainBoxHeight);
+    if (explainBoxHeight > '15em') {
         explainBox.style.maxWidth = '20em';
         console.log('Display optimization');
     }
-}
+} */
 
 topics[0].onmousedown = function() {
     explainBox.innerHTML = '<p>HTML provides the structure for a webpage.</p>';
-    displayOpt();
 }
 topics[1].onmousedown = function() {
     explainBox.innerHTML = "<p>CSS allows us to style and position elements on a webpage. The color of this text was set by a CSS rule.</p>";
-    displayOpt();
 }
 topics[2].onmousedown = function() {
     explainBox.innerHTML = '<p>JavaScript, shortened to js, adds functionality to a webpage. It is what inserted this text into this box.</p>';
-    displayOpt();
 }
 topics[3].onmousedown = function() {
     explainBox.innerHTML = '<p>Git is the industry-standard Version Control Software(VCS). It helps developers keep track of their files. Github is an online hosting service that can work with Git on local devices to allow teams to collaborate through the cloud.</p>'
-    displayOpt();
 }
 topics[4].onmousedown = function() {
     explainBox.innerHTML = '<p>Responsive web design is configuring a website to respond to changes in screen size. Devs use these design processes to make their websites look nice on all sizes and shapes of screens. A properly resposive website can look entirely different on a mobile device than on a laptop or monitor.</p>'
-    displayOpt();
 }
 
 // Contact button
